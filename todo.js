25baffe2be7b0e8b5da36fa339eeb70a82cf877f
Ronaldo Kwan <ronaldokwan17@gmail.com>
2024-02-01 11:58:58 +0700

    save1

--- todo.js
+++ todo.js
@@ -1,1 +1,14 @@
 // your commands in here
+const input = process.argv;
+const command = input[2];
+const command2 = input[3];
+const Controller = require("./controllers/controller");
+if (command === "help" || !command) {
+  Controller.help();
+} else if (command === "list" && command2 !== undefined) {
+  Controller.list();
+} else if (command === "add" && command2 !== undefined) {
+  Controller.add(command2);
+} else {
+  Controller.error();
+}
